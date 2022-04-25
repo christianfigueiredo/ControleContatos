390013bfd821106f620e1a5e17631e8455c41319
ChristianFiqueiredo <developer.figueiredo@gmail.com>
2022-04-25 14:35:09 +0100

    projeto mvc e criação do controler contato

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ControleContatos.Models;
+
+namespace ControleContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
