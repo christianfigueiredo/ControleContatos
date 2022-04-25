19aa85f09f37fa0814e9474948cc2f86e72b9e34
ChristianFiqueiredo <developer.figueiredo@gmail.com>
2022-04-25 17:08:22 +0100

    criação do frontend

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -15,6 +15,19 @@ namespace ControleContatos.Controllers
         {
             return View();
         }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
 
        
     }
